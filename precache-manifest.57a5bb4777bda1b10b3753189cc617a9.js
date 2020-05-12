de917bcc1e375ed7661de86da49b5363108bbac5
VacaSan <kotaracvladan@gmail.com>
2020-05-12 14:58:39 +0000

    Deploying to gh-pages from  @ c77040d05c08259e0eb9ba5f56f8ae25ab1da788 🚀

---
+++ precache-manifest.57a5bb4777bda1b10b3753189cc617a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cce6e55a1181c25c44f0b093bc9ecfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "047116fb63ac7db2faa3",
+    "url": "/static/css/2.07de6c03.chunk.css"
+  },
+  {
+    "revision": "0dcc5aae2dac21d440e8",
+    "url": "/static/css/main.4676a32b.chunk.css"
+  },
+  {
+    "revision": "047116fb63ac7db2faa3",
+    "url": "/static/js/2.43744e6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43744e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dcc5aae2dac21d440e8",
+    "url": "/static/js/main.2265596e.chunk.js"
+  },
+  {
+    "revision": "3b5bb56c23962bee1240",
+    "url": "/static/js/runtime-main.51f8bb20.js"
+  }
+]);
