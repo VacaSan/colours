3946613b58f388be7ec98b04d16d6dc2adb20a42
VacaSan <kotaracvladan@gmail.com>
2020-05-12 15:13:41 +0000

    Deploying to gh-pages from  @ 2a6fb0ee64e3b960790f8b8c4f3b987e9f2eb303 🚀

---
+++ precache-manifest.dad5bdedfa1aa1277e8a6082b2c67152.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b6c1f083f8db3b0c8082d84efe1fde",
+    "url": "/colours/index.html"
+  },
+  {
+    "revision": "0405fba215657a112288",
+    "url": "/colours/static/css/2.07de6c03.chunk.css"
+  },
+  {
+    "revision": "40639670b037e7c19818",
+    "url": "/colours/static/css/main.4676a32b.chunk.css"
+  },
+  {
+    "revision": "0405fba215657a112288",
+    "url": "/colours/static/js/2.258f97b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colours/static/js/2.258f97b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40639670b037e7c19818",
+    "url": "/colours/static/js/main.5ce3961d.chunk.js"
+  },
+  {
+    "revision": "41aa20936f19dd018340",
+    "url": "/colours/static/js/runtime-main.fd47a5e0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57a5bb4777bda1b10b3753189cc617a9.js"
+  "/colours/precache-manifest.dad5bdedfa1aa1277e8a6082b2c67152.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/colours/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
