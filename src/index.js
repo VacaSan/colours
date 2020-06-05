1a1ebc49d84b4d5b8aa73843f01b2b646d0ba2fd
VacaSan <kotaracvladan@gmail.com>
2020-06-06 00:06:57 +0800

    use toaster for user feedback

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { AppProviders } from "./context";
 
 render(
   <React.StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </React.StrictMode>,
   document.getElementById("root")
 );
